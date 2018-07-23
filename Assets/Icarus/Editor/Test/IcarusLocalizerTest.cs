e1e087ce66fa8a615a05ee889a2fff7552f6824e
Masataka Kohagura <masalinkinpark@gmail.com>
2018-07-23 12:40:11 +0900

    引数に string を入れれるように修正

--- Assets/Icarus/Editor/Test/IcarusLocalizerTest.cs
+++ Assets/Icarus/Editor/Test/IcarusLocalizerTest.cs
@@ -11,6 +11,7 @@ namespace Icarus.Editor.Test
             Assert.AreNotEqual(file, null);
         }
 
+        [Test]
         public void ConvertTextToDic()
         {
             TextLocalizer.Initialize("ja", "key,ja,en\nKeyTest,テスト,Test\n");
@@ -18,6 +19,13 @@ namespace Icarus.Editor.Test
 
             TextLocalizer.Initialize("en", "key,ja,en\nKeyTest,テスト,Test\n");
             Assert.AreEqual(TextLocalizer.GetText("KeyTest"), "Test");
+        }
+
+        [Test]
+        public void GetTextWithArgsTest()
+        {
+            TextLocalizer.Initialize("ja", "key,ja\nKeyTest,テスト{0}{1}{2}\n");
+            Assert.AreEqual(TextLocalizer.GetText("KeyTest", "引数", "の", "Test"), "テスト引数のTest");
         }
     }
 }
