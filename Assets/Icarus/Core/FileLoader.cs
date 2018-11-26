3538287bf8cb8b0bb6b79beaf17ca64aedbe5e0e
Masataka Kohagura <masalinkinpark@gmail.com>
2018-11-26 17:34:17 +0900

    リファクタリングとテストコードの追加

--- Assets/Icarus/Core/FileLoader.cs
+++ Assets/Icarus/Core/FileLoader.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
+using System.IO;
+using System;
 
 namespace Icarus.Core
 {
     public static class FileLoader
     {
-        private const string TextPath = "LocalizeText";
+        private const string TextPath = "LocalizedText";
 
-        public static TextAsset Load() => Resources.Load<TextAsset>(TextPath);
+        public static TextAsset LoadDefaultFile() => Resources.Load<TextAsset>(TextPath);
+
+        public static string LoadFile(string filePath)
+        {
+            var text = "";
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                text = sr.ReadToEnd();
+            }
+            return text;
+        }
     }
 }
